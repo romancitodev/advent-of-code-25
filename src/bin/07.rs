5efd454e5bac123dd255a34cd5b9d5415bd5ed0f
romandev <84428770+romancitodev@users.noreply.github.com>
2026-02-01 00:42:16 -0300

    perf: ⚡️ improve perf on day 07 part 02

--- src/bin/07.rs
+++ src/bin/07.rs
@@ -1,8 +1,6 @@
-use std::collections::{HashMap, HashSet};
+use std::collections::HashSet;
 
 advent_of_code::solution!(7);
-
-type Memo = HashMap<(usize, usize), u64>;
 
 const SPLITTER: u8 = b'^';
 
@@ -44,34 +42,47 @@ pub fn part_one(input: &str) -> Option<u64> {
     Some(splits)
 }
 
-fn split(ctx: &[&[u8]], ptr: usize, depth: usize, memo: &mut Memo) -> u64 {
-    if depth > ctx.len() - 1 {
-        return 0;
-    }
-
-    if let Some(r) = memo.get(&(ptr, depth)) {
-        return *r;
-    }
-
-    if ctx[depth][ptr] == SPLITTER {
-        let result =
-            1 + split(ctx, ptr - 1, depth + 1, memo) + split(ctx, ptr + 1, depth + 1, memo);
-        memo.insert((ptr, depth), result);
-        return result;
-    } else {
-        return split(ctx, ptr, depth + 1, memo);
-    };
-}
-
 pub fn part_two(input: &str) -> Option<u64> {
     let lines: Vec<&[u8]> = input.lines().map(|l| l.as_bytes()).collect();
     let init = lines[0]
         .iter()
         .position(|s| *s == b'S')
         .expect("Expected the initialized bean");
-    let mut paths = HashMap::new();
-    let beans = split(&lines, init, 1, &mut paths) + 1;
-    Some(beans)
+
+    let h = lines.len();
+    let w = lines[0].len();
+
+    let mut dp = vec![vec![0u64; w]; h];
+    dp[0][init] = 1;
+    let mut splits = 0;
+
+    for y in 0..h - 1 {
+        for x in 0..w {
+            let v = dp[y][x];
+            if v == 0 {
+                continue;
+            }
+
+            match lines[y][x] {
+                SPLITTER => {
+                    splits += v;
+                    if x > 0 {
+                        dp[y + 1][x - 1] += v;
+                    }
+
+                    if x + 1 < w {
+                        dp[y + 1][x + 1] += v;
+                    }
+                }
+
+                _ => {
+                    dp[y + 1][x] += v;
+                }
+            }
+        }
+    }
+
+    Some(splits + 1)
 }
 
 #[cfg(test)]
